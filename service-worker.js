9cf3be0a6b72ec35601f87ff5432a93b60066a6d
fzayniddinov71-create <fzayniddinov71@gmail.com>
2026-01-30 17:21:22 +0500

    Add service worker for caching and fetch handling

---
+++ service-worker.js
@@ -0,0 +1,14 @@
+self.addEventListener('install', e => {
+e.waitUntil(
+caches.open('weather-cache').then(cache =>
+cache.addAll(['/', '/index.html', '/style.css', '/script.js'])
+)
+);
+});
+
+
+self.addEventListener('fetch', e => {
+e.respondWith(
+caches.match(e.request).then(res => res || fetch(e.request))
+);
+});
